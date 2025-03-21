397f84b3069f36cc191bacf9b1df090c0589cffe
lionel staricoff <staricofflionel@hotmail.com>
2025-03-21 10:48:48 -0500

    fix:(MainView) regresando la logica inicial java

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -21,11 +20,26 @@ public class MainView extends VerticalLayout {
     public MainView(GreetService service) {
 
         // Use TextField for standard text input
-        Div textField = new Div();
-        textField.addClassName("root");
+        TextField textField = new TextField("Your name");
+        textField.addClassName("bordered");
 
+        // Button click listeners can be defined as lambda expressions
+        Button button = new Button("Say hello", e -> {
+            add(new Paragraph(service.greet(textField.getValue())));
+        });
 
+        // Theme variants give you predefined extra styles for components.
+        // Example: Primary button has a more prominent look.
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        add(textField);
+        // You can specify keyboard shortcuts for buttons.
+        // Example: Pressing enter in this view clicks the Button.
+        button.addClickShortcut(Key.ENTER);
+
+        // Use custom CSS classes to apply styling. This is defined in
+        // styles.css.
+        addClassName("centered-content");
+
+        add(textField, button);
     }
 }
